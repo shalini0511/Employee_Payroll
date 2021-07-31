8962e8332145d778d26ab2c6eb8986f5ce95d7c6
shalini <shalini.v@tvsnext.io>
2021-07-31 20:04:50 +0530

    [shalini]Inserting values in table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,10 +1,7 @@
---UC-1 creating database
-create database EmployeeServices;
---UC-2 creating Table
-create Table empoyee_payrolls
-(
-id int identity(1,1) primary key,
-name varchar(200) not null,
-salary float,
-startDate date
-)
+--UC-3 Insert values in Table
+Insert into empoyee_payroll(name,salary,startDate) values
+('shalini',20000,'2021-03-12'),
+('Magesh',25000,'2021-04-18'),
+('Gayathri',10000,'2020-05-13'),
+('Aruna',30000,'2020-08-19');
+select * from empoyee_payroll;
