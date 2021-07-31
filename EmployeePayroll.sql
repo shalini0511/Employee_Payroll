a7e7d5d9ec215dab105b09b1ec570bbe8c9dafd4
shalini <shalini.v@tvsnext.io>
2021-07-31 20:19:34 +0530

    [shalini]retrieving using cast and getdata

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -6,3 +6,6 @@ Insert into empoyee_payroll(name,salary,startDate) values
 ('Aruna',30000,'2020-08-19');
 --UC-4 Retrieve All data--
 select * from empoyee_payroll;
+------- UC 5: Select Query using Cast() an GetDate() -------
+select salary from empoyee_payroll where name='Magesh';
+select salary from empoyee_payroll where startDate BETWEEN Cast('2020-12-20' as Date) and GetDate();
